52768fd4d2790ffe5c6955ec2219955cbd9024bf
root <root@DESKTOP-QIRGNKT.localdomain>
2021-02-25 17:02:56 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '99e5732024d9331040dc5a09dbbd6df4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '019e629a-db6a-4dce-9cea-03d9ef4ea1c3'
+};
